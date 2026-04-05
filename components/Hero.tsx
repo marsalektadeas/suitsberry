ecac1b3893bd28aa5fc57d1ad9a02a15dccf4ba7
Tadeas Marsalek <marsalektadeas@gmail.com>
2026-04-05 09:03:27 +0200

    fix: remove SCROLL text from hero indicator

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -86,8 +86,7 @@ export default function Hero() {
       </div>
 
       {/* Scroll indicator */}
-      <div className="absolute bottom-8 left-1/2 -translate-x-1/2 flex flex-col items-center gap-2 text-white/30">
-        <span className="text-sm tracking-[0.2em] uppercase">Scroll</span>
+      <div className="absolute bottom-8 left-1/2 -translate-x-1/2 flex flex-col items-center gap-2 text-white/30 md:flex">
         <div className="w-px h-8 bg-white/20 animate-pulse" />
       </div>
     </section>
